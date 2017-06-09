58833d8c19efb7eab9a9170c417cddc78aa53de8
upendar245 <upendar245@users.noreply.github.com>
2017-06-09 11:35:42 +0530

    this for creating sample functions

--- lab4.py
+++ lab4.py
@@ -1,2 +1,11 @@
 #!/python3/bin/python3
+def rectangle(length,breadth)
+area=length*breadth
+permiter=2*(length+breadth)
+return(area)
 
+print("enter length")
+l=input()
+print("enter breadth")
+b=input()
+rectangle(l,b)
